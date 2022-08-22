02f8b39322b5c656b1a8cfea727178df15741631
CasinGit <107905043+CasinGit@users.noreply.github.com>
2022-08-22 21:58:43 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  document.title = "유기동물 조회서비스";
+  return (
+    <div className="App">
+      <p>
+        공사중
+      </p>
+    </div>
+  );
+}
+
+export default App;
